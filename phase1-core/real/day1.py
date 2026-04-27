63438981bc98df8be73309cc74cf605e0198cda1
Amalu <amalamal6034@gmail.com>
2026-04-27 23:06:11 +0530

    Numbers

---
+++ phase1-core/real/day1.py
@@ -0,0 +1,10 @@
+number = int(input('Enter the number: '))
+
+if number >0:
+    print("Positive")
+elif number < 0:
+    print("Negative")
+elif number == 0:
+    print("Number is zero ")
+else:
+    print("Unkown")            
